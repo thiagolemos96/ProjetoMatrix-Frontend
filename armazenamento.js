cf6d4b9978d3d06160e2fc31868ec65591bee1db
Thiago Lemos <thiagolemos1996@gmail.com>
2018-08-13 23:29:14 -0300

    adição de uma nova função para editar os participantes

--- armazenamento.js
+++ armazenamento.js
@@ -13,20 +13,31 @@ function ArmazenamentoLocal(key){
     }
 
     function desfragmentar(){
+
         var itemF = localStorage.getItem(key);
+
         return JSON.parse(itemF);
+
     }
 
     function fragmentar(participantes){
+
         var itemF = JSON.stringify(participantes);
+
         localStorage.setItem(key, itemF);
+
     }
 
     function removerLocal(propriedade, valor){
+
         var itens = desfragmentar();
+
         var index = itens.findIndex((elemento) => {
+
             return elemento[propriedade] === valor;
+
         });
+
         itens.splice(index, 1);
         fragmentar(itens);
     }
@@ -36,30 +47,40 @@ function ArmazenamentoLocal(key){
         var itens = desfragmentar();
         
         var index = itens.findIndex((elemento) => {
+
             return elemento[propriedade] === item[propriedade]
+
         });
         
         itens[index] = item;
-        
         fragmentar(itens);
     }
 
     function obterItem(propriedade, valor) {
+
         var itens = desfragmentar();
-        return itens.find((elemento) => {
+        return itens.find(function(elemento){
+
              return elemento[propriedade] === valor; }
+
             );
     }
 
     function obterItens(propriedade, valor) {
+
         var itens = desfragmentar();
+
         return itens.find((elemento) => { 
+
             return elemento[propriedade] === valor; 
+
         })
     }
 
     function obterTodosOsItens(){
+
         return desfragmentar();
+
     }
 
     return {
--- intermediario.js
+++ intermediario.js
@@ -1,6 +1,7 @@
 var sistema = new SistemaCadastro();
 
 var sexoSelecionado;
+var editor = false;
 
 
 window.onload = function() { //ao carregar a pagina chama essa função
@@ -8,15 +9,47 @@ window.onload = function() { //ao carregar a pagina chama essa função
 };
 
 function carregaDadosLocalStorageNaTabela() {
+    var sexoParticipante;
+    var situacaoParticipante;
     sistema.obterParticipantes().forEach(function(participantes, index){
-        document.getElementById("tabela").innerHTML += '<td>'+ participantes.nome +' '+ participantes.sobrenome+ '</td><td>' + participantes.idade + '</td><td>' + participantes.sexo + '</td><td>' + participantes.nota + '</td><td>' + participantes.aprovado + '</td><td>' + '<a href="javascript:void(0)" onclick="editarDados(\'' + participantes.email + '\')">Editar</a>' + ' ' + '<a href="javascript:void(0)" onclick="excluirDados(\'' + participantes.email + '\')">Excluir</a>' + '</td></tr>'
+
+        if(participantes.sexo === 1){
+            sexoParticipante = 'Masculino';
+        }
+        else{
+            sexoParticipante = 'Feminino';
+        }
+
+        if(participantes.aprovado === true){
+            situacaoParticipante = 'Sim';
+        }
+        else{
+            situacaoParticipante = 'Não';
+        }
+
+        document.getElementById("tabela").innerHTML += '<td>'+ participantes.nome +' '+ participantes.sobrenome+ '</td><td>' + participantes.idade + '</td><td>' + sexoParticipante + '</td><td>' + participantes.nota + '</td><td>' + situacaoParticipante + '</td><td>' + '<a href="javascript:void(0)" onclick="editarDados(\'' + participantes.email + '\')">Editar</a>' + ' ' + '<a href="javascript:void(0)" onclick="excluirDados(\'' + participantes.email + '\')">Excluir</a>' + '</td></tr>'
 	});
 
 }
 
+//console.log(editor);
+
 function editarDados(email) { 
-    
-}
+    editor = true;
+
+    var participante = sistema.obterParticipante(email);
+    var form = document.querySelector("#formulario");
+
+    form.nome.value = participante.nome;
+    form.sobrenome.value = participante.sobrenome;
+    form.email.value = participante.email;
+    form.idade.value = participante.idade;    
+    form.nota.value = participante.nota;
+    form.sexo.value = participante.sexo;
+
+    form.email.disabled = true;
+
+  }
 
 function excluirDados(email) {
     sistema.removerParticipante(email);
@@ -26,7 +59,6 @@ function excluirDados(email) {
 }
 
 var salvarParticipante = document.getElementById("btnCadastro").addEventListener("click",function (){
-
     if(document.getElementById("masculino").checked) {
         sexoSelecionado = 1;
     }
@@ -34,20 +66,39 @@ var salvarParticipante = document.getElementById("btnCadastro").addEventListener
         sexoSelecionado = 2;
     } //verifica qual sexo foi selecionado
 
+    if(editor === false){
+
     try {
-    sistema.adicionarParticipante(
-    document.getElementById("nome").value,
-    document.getElementById("sobrenome").value, 
-    document.getElementById("email").value,
-    document.getElementById("idade").value,
-    sexoSelecionado);
+        sistema.adicionarParticipante(
+            document.getElementById("nome").value,
+            document.getElementById("sobrenome").value, 
+            document.getElementById("email").value,
+            document.getElementById("idade").value,
+        sexoSelecionado);
 
     } catch(Error){
         window.alert(Error.message);}
         
-    sistema.adicionarNotaAoParticipante(
-    document.getElementById("email").value,
-    document.getElementById("nota").value);
-    window.location.reload(true);
+        sistema.adicionarNotaAoParticipante(
+            document.getElementById("email").value,
+            document.getElementById("nota").value);
 
+        window.location.reload(true);
+
+        editor = true;
+    }
+
+    else{
+        sistema.atualizarParticipante( 
+            document.getElementById("nome").value,
+            document.getElementById("sobrenome").value, 
+            document.getElementById("email").value,
+            document.getElementById("idade").value,
+            sexoSelecionado,
+            document.getElementById("nota").value)
+
+        window.location.reload(true);
+
+        editor = false;
+    }
 });
